20386002b40a8546e5bc9e9b2aeb1275de5bf83b
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-05 15:04:49 -0800

    fix the bug when live server didn't start from template

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import Age from './calculator.js';
+import '.././css/styles.css';
+// import Age from './calculator.js';
+
+
+$('.btn').click(function(event1) {
+  event1.preventDefault();
+  alert('R');
+  // let gender = $('#gender').val();
+  // $('#output').text(gender);
+
+});
