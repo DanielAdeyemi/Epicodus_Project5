a8f730e713c85308ff331fabb63c65c7bbf11efc
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-05 10:51:22 -0800

    pass test 1 for creating Age object

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,8 +1,10 @@
 export default class Age {
+  constructor(age, gender) {
+    this.years = age;
+    this.expect = (gender === 'male') ? (76 - age) : (80 - age);
+  }
 
 }
-
 //   mercuryAge() {
 //     const age = this.years * .24
 //   }
-// }
