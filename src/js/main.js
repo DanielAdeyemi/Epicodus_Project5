a4f5ba522c1e10e10849fdf05ef1cfaf942d66fc
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-05 16:27:01 -0800

    check user input for errors and add field for name

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,10 @@ import Age from './calculator.js';
 $('.btn').click(function(event1) {
   event1.preventDefault();
   const clk = $(this).attr('id');
+  const name = $('#name').val();
   const age = $('#age').val();
   const gender = $('#gender').val();
-  if (!gender || gender === 'none' || !age || isNaN(age)) {
+  if (!gender || gender === 'none' || !age || isNaN(age) || !name) {
     $('form')[0].reset();
     return alert(`Make sure you don't leave empty fields and all information is correct!`);
   }
@@ -18,7 +19,7 @@ $('.btn').click(function(event1) {
 
   if (clk === 'Earth') {
     $('.card-title').text(`${clk}`);
-    $('.card-text').html(`<p>You are ${spaceAge.years} years old on ${clk}</p>
+    $('.card-text').html(`<p>${name}, you are ${spaceAge.years} years old on ${clk}</p>
     <p>Approximate time left to live for you - ${spaceAge.expect} years`);
   } else {
     alert("later");
